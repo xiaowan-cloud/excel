8e988a29bde86080a486d19555fbd0c6cdb8ea7c
张万 <2439881583@qq.com>
2021-08-12 15:49:39 +0800

    Initial commit

---
+++ src/main/java/wan/com/templateproject/TemplateProjectApplication.java
@@ -0,0 +1,13 @@
+package wan.com.templateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateProjectApplication.class, args);
+    }
+
+}
