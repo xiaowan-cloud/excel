f073fd2c7dce5dfdb8d07c2b1a4bb1c817c3e8f4
张万 <2439881583@qq.com>
2021-08-17 14:06:21 +0800

    加个实体

---
+++ src/main/java/wan/com/templateproject/entity/people.java
@@ -0,0 +1,23 @@
+package wan.com.templateproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author wan
+ * @version 1.0.0
+ * @Description people实体
+ * @createTime 2021年08月17日 14:03:44
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class people {
+    private int id;
+    private String name;
+    private int age;
+    //0:男,1:女
+    private int gender;
+    private String hobby;
+}
